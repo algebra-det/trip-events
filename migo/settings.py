8b7876e01157b2e8a911a0752d4104a0f75b8291
Akash Chauhan <akash.th4kur@gmail.com>
2024-02-23 15:43:30 +0530

    allowed_hosts

--- migo/settings.py
+++ migo/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY =  os.environ.get('SECRET_KEY', 'Some-Secret-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'hosting1django.pythonanywhere.com']
+ALLOWED_HOSTS = ['hosting1django.pythonanywhere.com', 'localhost']
 
 
 # Application definition
